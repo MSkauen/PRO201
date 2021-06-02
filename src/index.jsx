51a981b69608833a91bd5c9ba31566ab96fcfed5
Mskauen <mathias.skauen.harestad@gmail.com>
2021-06-02 18:41:06 +0200

    Getting AppWatchCourse ready to display correct data

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Application } from "./Application";
-import "../shared/css/stylesheet.css";
+import { Application } from "./client/Application";
+import "./shared/css/stylesheet.css";
 
 ReactDOM.render(<Application />, document.getElementById("root"));
