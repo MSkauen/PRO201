f4db1905349883c12caddb2e57e2201ab3865f6a
Mskauen <mathias.skauen.harestad@gmail.com>
2022-03-26 22:51:22 +0100

    Using herokus config sets

--- src/db/db.js
+++ src/db/db.js
@@ -1,11 +1,10 @@
 
 const mongoose = require('mongoose')
 const schemas = require('./schemas')
-const mongodb = require('./dbconfig')
 
 const connectDB = async ( ) => {
     try {
-        await mongoose.connect(mongodb.mongodbpasscode, {
+        await mongoose.connect(process.env.mongodbpasscode, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false,
